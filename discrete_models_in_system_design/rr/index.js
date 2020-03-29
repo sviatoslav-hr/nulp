594d8924baa8358fc9e928eed971c7c56aa160f8
Sviatoslav Khrystyna <sviatoslavxp@gmail.com>
2020-03-29 17:25:02 +0300

    Fix package.json in Discrete models

--- discrete_models_in_system_design/rr/index.js
+++ discrete_models_in_system_design/rr/index.js
@@ -1,14 +1,14 @@
 const hgAlgorithm = require('./hg-alghoruthm');
 
 // Square
-const test1 = [
+const matrix1 = [
   [10, 19, 8, 15],
   [10, 18, 7, 17],
   [13, 16, 9, 14],
   [14, 17, 10, 19]
 ];
 // Tall
-const test2 = [
+const matrix2 = [
   [10, 19, 8, 15],
   [10, 18, 7, 17],
   [13, 16, 9, 14],
@@ -16,16 +16,16 @@ const test2 = [
   [14, 17, 10, 19]
 ];
 // Wide
-const test3 = [
+const matrix3 = [
   [10, 19, 8, 15, 14],
   [10, 18, 7, 17, 17],
   [13, 16, 9, 14, 10],
   [12, 19, 8, 18, 19]
 ];
 
-console.log(hgAlgorithm(test1, "min"));
-console.log(hgAlgorithm(test1, "max"));
-console.log(hgAlgorithm(test2, "min"));
-console.log(hgAlgorithm(test2, "max"));
-console.log(hgAlgorithm(test3, "min"));
-console.log(hgAlgorithm(test3, "max"));
+console.log(hgAlgorithm(matrix1, "min"));
+console.log(hgAlgorithm(matrix1, "max"));
+console.log(hgAlgorithm(matrix2, "min"));
+console.log(hgAlgorithm(matrix2, "max"));
+console.log(hgAlgorithm(matrix3, "min"));
+console.log(hgAlgorithm(matrix3, "max"));
